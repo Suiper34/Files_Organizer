31bece1192e00129a5744ce9fc85e7eddf2b0a2a
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-11-09 16:31:43 +0000

    initialize script

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from main import organize_files_by_extension
+
+
+if __name__ == '__main__':
+    organize_files_by_extension(
+        source_dir=r'C:\User\user\suip_files',
+        destination_dir=r'D:\Suip_Organized\Organized_files'
+    )
